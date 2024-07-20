8f9cbffdc8a99c26bfe8535900f445a4fc7f5a86
Rodrigo Gonzalez <zrodrigo_gonzalez_123@hotmail.com>
2024-07-20 18:10:36 -0300

    Se agrego logica al CitaController
    git push

--- src/main/java/com/proymedic/consultoriomedico/Controllers/CitaController.java
+++ src/main/java/com/proymedic/consultoriomedico/Controllers/CitaController.java
@@ -1,4 +1,23 @@
 package com.proymedic.consultoriomedico.Controllers;
 
+import com.proymedic.consultoriomedico.Entities.Cita;
+import com.proymedic.consultoriomedico.Service.impl.ICitaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cita")
 public class CitaController {
+    @Autowired
+    private ICitaService iCitaService;
+
+    @GetMapping("/find")
+    public List<Cita> findall(){
+        return iCitaService.findAllCita();
+    }
 }
