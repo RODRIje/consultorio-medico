1593ab76ee8c2d595d0b9b69232c01b1a5414414
Rodrigo Gonzalez <zrodrigo_gonzalez_123@hotmail.com>
2024-07-21 17:14:09 -0300

    Se mejoro el metodo save en HorarioDisponibleController

--- src/main/java/com/proymedic/consultoriomedico/Controllers/HorarioDisponibleController.java
+++ src/main/java/com/proymedic/consultoriomedico/Controllers/HorarioDisponibleController.java
@@ -3,8 +3,9 @@ package com.proymedic.consultoriomedico.Controllers;
 import com.proymedic.consultoriomedico.Controllers.dto.HorarioDisponibleDTO;
 import com.proymedic.consultoriomedico.Entities.HorarioDisponible;
 import com.proymedic.consultoriomedico.Entities.Medico;
-import com.proymedic.consultoriomedico.Persistence.impl.IMedicoDAO;
+
 import com.proymedic.consultoriomedico.Service.impl.IHorarioDisponibleService;
+import com.proymedic.consultoriomedico.Service.impl.IMedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class HorarioDisponibleController {
     private IHorarioDisponibleService iHorarioDisponibleService;
 
     @Autowired
-    private IMedicoDAO iMedicoDAO;
+    private IMedicoService iMedicoService;
 
     @GetMapping("/find")
     public List<HorarioDisponible> findAll(){
@@ -31,7 +32,7 @@ public class HorarioDisponibleController {
     public ResponseEntity<?> save(@RequestBody HorarioDisponibleDTO horarioDisponibleDTO) throws URISyntaxException {
 
         HorarioDisponible horario = new HorarioDisponible();
-        Medico medico = iMedicoDAO.findById(horarioDisponibleDTO.getId());
+        Medico medico = iMedicoService.findById(horarioDisponibleDTO.getMedico());
 
         horario.setDiaSemana(horarioDisponibleDTO.getDiaSemana());
         horario.setHoraInicio(horarioDisponibleDTO.getHoraInicio());
