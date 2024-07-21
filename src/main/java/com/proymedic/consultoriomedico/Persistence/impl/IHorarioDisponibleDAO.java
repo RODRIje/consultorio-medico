ab690014ce132e7b9c23fd2b3b8072ad85765b7e
Rodrigo Gonzalez <zrodrigo_gonzalez_123@hotmail.com>
2024-07-21 16:10:21 -0300

    Se completo la inyeccion de dependencia en IHorarioDisponibleDAO

--- src/main/java/com/proymedic/consultoriomedico/Persistence/impl/IHorarioDisponibleDAO.java
+++ src/main/java/com/proymedic/consultoriomedico/Persistence/impl/IHorarioDisponibleDAO.java
@@ -2,6 +2,7 @@ package com.proymedic.consultoriomedico.Persistence.impl;
 
 import com.proymedic.consultoriomedico.Entities.HorarioDisponible;
 import com.proymedic.consultoriomedico.Persistence.HorarioDisponibleDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalTime;
@@ -9,23 +10,25 @@ import java.util.List;
 
 @Component
 public class IHorarioDisponibleDAO implements HorarioDisponibleDAO {
+    @Autowired
+    private HorarioDisponibleDAO horarioDisponibleDAO;
     @Override
     public void saveHorarioDispo(HorarioDisponible horarioDisponible) {
-
+        horarioDisponibleDAO.saveHorarioDispo(horarioDisponible);
     }
 
     @Override
     public List<HorarioDisponible> findAllHorarios() {
-        return null;
+        return horarioDisponibleDAO.findAllHorarios();
     }
 
     @Override
     public void deleteHorarioDispo(Long id) {
-
+        horarioDisponibleDAO.deleteHorarioDispo(id);
     }
 
     @Override
     public HorarioDisponible updateHorario(Long id, String diaSemana, LocalTime horaInicio, LocalTime horaFin) {
-        return null;
+        return horarioDisponibleDAO.updateHorario(id, diaSemana, horaInicio, horaFin);
     }
 }
