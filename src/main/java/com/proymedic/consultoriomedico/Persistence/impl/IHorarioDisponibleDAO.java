c873990e37359ea6c40aee72e39bed4171a21f2d
Rodrigo Gonzalez <zrodrigo_gonzalez_123@hotmail.com>
2024-07-21 16:58:48 -0300

    Se arreglaron los errores de las inyecciones de dependencia en HorarioDisponibleDAO

--- src/main/java/com/proymedic/consultoriomedico/Persistence/impl/IHorarioDisponibleDAO.java
+++ src/main/java/com/proymedic/consultoriomedico/Persistence/impl/IHorarioDisponibleDAO.java
@@ -3,6 +3,7 @@ package com.proymedic.consultoriomedico.Persistence.impl;
 import com.proymedic.consultoriomedico.Controllers.dto.HorarioDisponibleDTO;
 import com.proymedic.consultoriomedico.Entities.HorarioDisponible;
 import com.proymedic.consultoriomedico.Persistence.HorarioDisponibleDAO;
+import com.proymedic.consultoriomedico.Repositories.HorarioDisponibleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,24 +13,25 @@ import java.util.List;
 @Component
 public class IHorarioDisponibleDAO implements HorarioDisponibleDAO {
     @Autowired
-    private HorarioDisponibleDAO horarioDisponibleDAO;
+    private HorarioDisponibleRepository horarioDisponibleRepository;
+
     @Override
     public void saveHorarioDispo(HorarioDisponible horarioDisponible) {
-        horarioDisponibleDAO.saveHorarioDispo(horarioDisponible);
+        horarioDisponibleRepository.save(horarioDisponible);
     }
 
     @Override
     public List<HorarioDisponible> findAllHorarios() {
-        return horarioDisponibleDAO.findAllHorarios();
+        return horarioDisponibleRepository.findAll();
     }
 
     @Override
     public void deleteHorarioDispo(Long id) {
-        horarioDisponibleDAO.deleteHorarioDispo(id);
+        horarioDisponibleRepository.deleteById(id);
     }
 
     @Override
     public HorarioDisponible updateHorario(Long id, String diaSemana, LocalTime horaInicio, LocalTime horaFin) {
-        return horarioDisponibleDAO.updateHorario(id, diaSemana, horaInicio, horaFin);
+        return null; // Crear metodo en el repository para que haga el update correspondiente
     }
 }
--- src/main/java/com/proymedic/consultoriomedico/Repositories/HorarioDisponibleRepository.java
+++ src/main/java/com/proymedic/consultoriomedico/Repositories/HorarioDisponibleRepository.java
@@ -2,8 +2,13 @@ package com.proymedic.consultoriomedico.Repositories;
 
 import com.proymedic.consultoriomedico.Entities.HorarioDisponible;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalTime;
 
 @Repository
 public interface HorarioDisponibleRepository extends JpaRepository<HorarioDisponible, Long> {
+   /* @Query
+    public HorarioDisponible update(Long id, String diaSemana, LocalTime horaInicio, LocalTime horaFin);*/
 }
