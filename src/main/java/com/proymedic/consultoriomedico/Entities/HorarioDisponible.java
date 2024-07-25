2bb1376e4437fca7a3ab5021f9c02676398ffec4
Rodrigo Gonzalez <zrodrigo_gonzalez_123@hotmail.com>
2024-07-25 15:01:58 -0300

    Se agrego el builder a la entidad HorarioDisponible

--- src/main/java/com/proymedic/consultoriomedico/Entities/HorarioDisponible.java
+++ src/main/java/com/proymedic/consultoriomedico/Entities/HorarioDisponible.java
@@ -2,6 +2,7 @@ package com.proymedic.consultoriomedico.Entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.time.LocalTime;
 
 @Entity
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "horario_disponible")
