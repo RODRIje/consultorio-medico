d6db8e6866793b82a87efe7b5cdce7956ce26b6d
Rodrigo Gonzalez <zrodrigo_gonzalez_123@hotmail.com>
2024-08-24 19:06:19 -0300

    Test finalizados para ClienteService

--- src/test/java/com/proymedic/consultoriomedico/Service/ClienteServiceTests.java
+++ src/test/java/com/proymedic/consultoriomedico/Service/ClienteServiceTests.java
@@ -1,6 +1,7 @@
 package com.proymedic.consultoriomedico.Service;
 
 import com.proymedic.consultoriomedico.Entities.Cliente;
+import com.proymedic.consultoriomedico.Entities.Medico;
 import com.proymedic.consultoriomedico.Repositories.ClienteRepository;
 import com.proymedic.consultoriomedico.Service.impl.IClienteService;
 import org.hibernate.mapping.Collection;
@@ -19,6 +20,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.willDoNothing;
@@ -240,6 +242,40 @@ public class ClienteServiceTests {
         assertThat(listCliente.get(0).getApellido()).isEqualTo("Varela");
         assertThat(listCliente.get(1).getNombre()).isEqualTo("Roman");
         assertThat(listCliente.get(1).getApellido()).isEqualTo("Riquelme");
+    }
+
+    @DisplayName("Test para actualizar un cliente por id")
+    @Test
+    void testUpdateClienteById(){
+        // given
+        Long clienteId = 1L;
+
+        Cliente clienteExistente = Cliente.builder()
+                .nombre("Roman")
+                .apellido("Varela")
+                .obraSocial(true)
+                .nombreObraSocial("IOMA")
+                .build();
+
+        Cliente clienteActualizado = Cliente.builder()
+                .nombre("Roman")
+                .apellido("Riquelme")
+                .obraSocial(true)
+                .nombreObraSocial("OSDE")
+                .build();
+
+        given(clienteRepository.findById(clienteId)).willReturn(Optional.of(clienteExistente));
+        given(clienteRepository.save(any(Cliente.class))).willAnswer(invocation -> invocation.getArgument(0));
+
+        // when
+        clienteService.updateCliente(clienteId, clienteActualizado);
+
+        // then
+        assertThat(clienteActualizado.getNombre()).isEqualTo("Roman");
+        assertThat(clienteActualizado.getApellido()).isEqualTo("Riquelme");
+        assertThat(clienteActualizado.getNombreObraSocial()).isEqualTo("OSDE");
+        assertThat(clienteActualizado.getObraSocial()).isEqualTo(true);
+        verify(clienteRepository).save(clienteActualizado);
     }
 
 }
